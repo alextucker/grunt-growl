9b72833e1bd1f4b3f284e668acb690c1d5eac424
Rob Burns <rburns@paiges.net>
2012-07-15 11:26:36 +0700

    default handling of headers, warning, and error logs

--- tasks/growl.js
+++ tasks/growl.js
@@ -8,7 +8,10 @@
 
 module.exports = function(grunt) {
 
-  var growl = require('growl');
+  var growl = require('growl'),
+      path = require('path'),
+      messages = [],
+      ignoreWatch = false;
 
   // Please see the grunt documentation for more information regarding task and
   // helper creation: https://github.com/cowboy/grunt/blob/master/docs/toc.md
@@ -28,6 +31,54 @@ module.exports = function(grunt) {
   grunt.registerHelper('growl', growlMessage);
 
   // ==========================================================================
+  // DEFAULT NOTIFICATIONS
+  // ==========================================================================
+
+  grunt.utils.hooker.hook(grunt.log, 'write', function(msg){
+    if( grunt.log.uncolor(msg).match(/Waiting.../) ) { flushMessages('ok'); }
+  });
+
+  grunt.utils.hooker.hook(grunt.log, 'header', function(msg){
+    msg = grunt.log.uncolor(msg);
+
+    if( ignoreWatch && msg.match(/"watch" task/) ) { return; }
+
+    if( msg.match(/".+:.+"/) ) { return; }
+
+    if( !ignoreWatch && msg.match(/"watch" task/) ) {
+      msg += ' for ' + path.basename(process.cwd());
+      ignoreWatch = true;
+    }
+
+    messages.push(msg);
+  });
+
+  grunt.utils.hooker.hook(grunt.log, 'ok', function(msg){
+    if( typeof msg === 'string' ) {
+     messages.push(grunt.log.uncolor(msg));
+    }
+  });
+
+  grunt.utils.hooker.hook(grunt, 'warn', function(error){
+    var warning = [];
+
+    if( typeof error !== 'undefined' ) {
+      warning.push(messages[0]);
+      warning.push(messages[messages.length-1]);
+      warning.push(String(error.message || error));
+      messages = warning;
+      flushMessages('error');
+    }
+  });
+
+  grunt.utils.hooker.hook(grunt.log, 'error', function(msg){
+    if( typeof msg === 'string' ) {
+     messages.push(grunt.log.uncolor(msg));
+     flushMessages('error');
+    }
+  });
+
+  // ==========================================================================
   // SHARED FUNCTIONS
   // ==========================================================================
 
@@ -35,4 +86,16 @@ module.exports = function(grunt) {
     growl(config.message, config);
   }
 
+  function flushMessages(status) {
+    if( messages.length <= 0 ) {
+      return;
+    }
+
+    growlMessage({
+      message: messages.join('\n'),
+      title: 'Grunt',
+      image: __dirname + '/../img/' + status + '.png'
+    });
+    messages = [];
+  }
 };
